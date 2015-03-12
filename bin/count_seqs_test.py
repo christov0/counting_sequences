548d395394c3b752b1f68d5ce291c1df33955fea
Peter van Heusden <pvh@sanbi.ac.za>
2015-03-12 10:00:42 +0200

    * Put try..except..else around assert

--- bin/count_seqs_test.py
+++ bin/count_seqs_test.py
@@ -10,5 +10,10 @@ except IOError as e:
   print >>sys.stderr, "Error opening file {}: {}".format(filename, e.strerror)
 else:
   seq_count = seq_utils.count_seqs(input_file)
-  assert seq_count == 94
-  print seq_count,"in",filename
+  # not really the right thing to do when you've got an assert.
+  try:    
+    assert seq_count == 94, "Expected 94 sequences in python.fasta"
+  except AssertionError:
+    print >>sys.stderr, "I got the wrong value from count_seqs, got {} expected 94".format(seq_count)
+  else:  
+    print seq_count,"in",filename
