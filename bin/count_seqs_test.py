853e06c78010fa6ccfe5a128758b8a48b1c20d35
pvanheus <pvh@webbedfeet.co.za>
2015-03-12 09:50:02 +0200

    * Use try..except..else to protect open call

--- bin/count_seqs_test.py
+++ bin/count_seqs_test.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
 import seq_utils
+import sys
 
 filename = 'python.fasta'
-input_file = open(filename)
-seq_count = seq_utils.count_seqs(input_file)
-print seq_count,"in",filename
+try:
+  input_file = open(filename)
+except IOError as e:
+  print >>sys.stderr, "Error opening file {}: {}".format(filename, e.strerror)
+else:
+  seq_count = seq_utils.count_seqs(input_file)
+  print seq_count,"in",filename
