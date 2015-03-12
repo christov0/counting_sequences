cbb7479487d918c767124a5c6f70c6b6066d0183
Chris van Niekerk <chrisvn@sun.ac.za>
2015-03-12 10:13:21 +0200

    chris_count_all_seqs

--- bin/count_all_seqs.py
+++ bin/count_all_seqs.py
@@ -1,1 +1,22 @@
-# An empty file
+#!/usr/bin/env python
+
+import sys
+import glob
+import seq_utils
+
+mydir = sys.argv[1]
+
+pattern = '*.fasta'
+fullpath = mydir + '/' + pattern
+filenames = glob.glob(fullpath)
+cnt=0
+for filedir in filenames:
+	tmp_file=open(filedir)
+	namesplit=filedir.partition(mydir)
+	filename=namesplit[len(namesplit)-1]
+	print filename,seq_utils.count_seqs(tmp_file)
+	cnt+=1
+
+if len(sys.argv)<1:
+	print >>sys.stderr,"To use tester.py enter 'tester.py <filename>'"
+	sys.exit(1)
--- lib/seq_utils.py
+++ lib/seq_utils.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python
+
 def count_seqs(input_file):
-    count = 0
-    for line in input_file:
-        line = line.lstrip() # strip leading spaces, if any
-        if line.startswith('>'):
-            count += 1
-    return count
+	count=0
+	for line in input_file:
+		line=line.strip() # strip any leading spaces
+		if line.startswith(">"):
+			count+=1
+	return count
